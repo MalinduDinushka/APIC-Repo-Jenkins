baffe35d16f630d76334b28d82f6d092800a303c
MalinduDinushka <mdhherath@gmail.com>
2026-05-12 14:45:01 +0530

    updated 2

--- scripts/createapp.bat
+++ scripts/createapp.bat
@@ -1,1 +1,1 @@
-apic apps:create --org api-connect-az --server https://platform-api.trial.apiconnect.automation.ibm.com --consumer-org Consumer_org1 --catalog development scripts\app_create.yaml --debug
+C:\APIC1\apic apps:create --org api-connect-az --server https://platform-api.trial.apiconnect.automation.ibm.com --consumer-org Consumer_org1 --catalog development scripts\app_create.yaml --debug
