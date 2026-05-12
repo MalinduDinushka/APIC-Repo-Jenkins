3ff45f48585b002617ddbb9c13e2b34f95d77e71
MalinduDinushka <mdhherath@gmail.com>
2026-05-12 11:58:40 +0530

    added accept license

--- scripts/login.bat
+++ scripts/login.bat
@@ -1,2 +1,5 @@
-C:\APIC1\apic client-creds:set credentials.json
-C:\APIC1\apic login --sso --context provider --server https://platform-api.trial.apiconnect.automation.ibm.com --apiKey a509f344-cc3d-4efe-90de-00647b63ae77
+@echo off
+
+C:\APIC1\apic client-creds:set credentials.json --accept-license
+
+C:\APIC1\apic login --sso --context provider --server https://platform-api.trial.apiconnect.automation.ibm.com --apiKey a509f344-cc3d-4efe-90de-00647b63ae77 --accept-license
