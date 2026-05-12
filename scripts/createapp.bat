e1986e6ef0bc363bca4ed406ba017a543ec39070
MalinduDinushka <mdhherath@gmail.com>
2026-05-12 15:06:20 +0530

    new 2

--- scripts/createapp.bat
+++ scripts/createapp.bat
@@ -1,1 +1,3 @@
-C:\APIC1\apic apps:create --org api-connect-az --server https://platform-api.trial.apiconnect.automation.ibm.com --consumer-org Consumer_org1 --catalog DEVELOPMENT scripts\appcreate.yaml --debug
+
+@echo off
+C:\APIC1\apic apps:create --org api-connect-az --server https://platform-api.trial.apiconnect.automation.ibm.com --consumer-org Consumer_org1 --catalog DEVELOPMENT %WORKSPACE%\scripts\appcreate.yaml --debug
